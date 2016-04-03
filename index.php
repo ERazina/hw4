8b042b65914f7808e74ee9a225e66d6bf20b1910
Elina <razina_elina@mail.ru>
2016-04-03 16:26:10 +0300

    new things

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <head>
 <link rel = "stylesheet" href="css.css" />
-    
 <script>
-    function insertTag(textarea, tagStart, tagEnd) { 
-    var edit = document.getElementsByName(text).item(0);
-    if (document.getSelection) {
-        edit.value = edit.value.substring(0, edit.selectionStart) +
-        tagStart +
-        edit.value.substring(edit.selectionStart, edit.selectionEnd) +
-        tagEnd +
-        edit.value.substring(edit.selectionEnd, edit.value.length);
-    }
-        console.log(edit);
-    }
+function insertTag(text, tagStart, tagEnd) { 
+var edit = document.getElementsByName(text).item(0);
+if (document.getSelection) {
+edit.value = edit.value.substring(0, edit.selectionStart) +
+tagStart +
+edit.value.substring(edit.selectionStart, edit.selectionEnd) +
+tagEnd +
+edit.value.substring(edit.selectionEnd, edit.value.length);
+}
+console.log(edit);
+}
+
     
 </script>
 </head>
+
 
 <body>
     <h1>Онлайн текстовый редактор</h1>
@@ -25,9 +26,9 @@
     <div id = "filename">Введите название файла <input type="text" name="name"></div>
     <h2>Введите текст</h2>
     <div id = "buttons">
-        <input type="button" value="b" onclick="insertTag('textarea','[b]','[/b]')"> 
-        <input type="button" value="i" onclick="insertTag('textarea','[i]','[/i]')"> 
-        <input type="button" value="u" onclick="insertTag('textarea','[u]','[/u]')"><br> 
+        <input type="button" value="b" onclick="insertTag('text','[b]','[/b]')"> 
+        <input type="button" value="i" onclick="insertTag('text','[i]','[/i]')"> 
+        <input type="button" value="u" onclick="insertTag('text','[u]','[/u]')"><br> 
     </div>
     <textarea rows="25" cols="100" name="text" id="textarea"></textarea></br>
     <input type="submit" value="Сохранить">
